fa19dd9a1fca530d13881460845632301fce5484
webcontabil-2026 <tcceducare2026@gmail.com>
2026-04-08 19:45:53 -0300

    primeiras alterações nas blades..

---
+++ resources/views/Inicio.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WC teste</title>
+    <link rel="stylesheet" href="{{ asset('css/style_inicio.css') }}">
+</head>
+<body>
+
+</body> 
