88f349b75d764c04201cfe3c26e78f2070f6989d
webcontabil-2026 <tcceducare2026@gmail.com>
2026-04-15 11:36:28 -0300

    Ajustes - lucca 15.04

--- resources/views/Inicio.blade.php
+++ resources/views/Inicio.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>WC teste</title>
-    <link rel="stylesheet" href="{{ asset('css/style_inicio.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/sla.css') }}">
 </head>
 <body>
 
--- resources/views/teste.blade.php
+++ resources/views/teste.blade.php
@@ -4,7 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Teste com Bootstrap</title> 
+    
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<link href="{{ asset('css/page1.css') }}" rel="stylesheet">  <!-- link do CSS personalizado -->
 </head>
 <body>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
