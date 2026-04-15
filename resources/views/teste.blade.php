a994e69bbdfdc4f21e8e5499d261050affe424b5
webcontabil-2026 <tcceducare2026@gmail.com>
2026-04-15 11:42:13 -0300

    teste de alterações

--- resources/views/teste.blade.php
+++ resources/views/teste.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Teste com Bootstrap</title> 
-    
+
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
 
 <link href="{{ asset('css/page1.css') }}" rel="stylesheet">  <!-- link do CSS personalizado -->
@@ -28,7 +28,7 @@
           </a>
           <ul class="nav nav-pills">
             <li class="nav-item">
-              <a href="#" class="nav-link active" aria-current="page">Home</a>
+              <a href="#" class="nav-link active" aria-current="page">casa sla</a>
             </li>
             <li class="nav-item"><a href="#" class="nav-link">funcão</a></li>
             <li class="nav-item"><a href="#" class="nav-link">Pricing</a></li>
