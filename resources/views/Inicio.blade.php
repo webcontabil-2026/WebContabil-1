2b0166d06ce45636934301c47f84b3d6ab323691
erick sanches <sanchesalveserick@gmail.com>
2026-04-27 11:53:32 -0300

    git commit -m "erick teste"

--- resources/views/Inicio.blade.php
+++ resources/views/Inicio.blade.php
@@ -8,5 +8,8 @@
     <link rel="stylesheet" href="{{ asset('css/sla.css') }}">
 </head>
 <body>
-
+<div>
+    <h1>Bem-vindo ao WC teste</h1>
+    <p>Este é um exemplo de página inicial usando Laravel Blade.</p>
+</div>
 </body> 
